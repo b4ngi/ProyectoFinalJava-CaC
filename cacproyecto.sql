ec93c749ae72b36548d62f7b11de5f8484a1f563
b4ngi <ignaciobenetucci@gmail.com>
2022-12-27 20:30:36 -0300

    Se agrega archivo .sql

---
+++ cacproyecto.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-12-2022 a las 08:32:56
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cacproyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidades`
+--
+
+CREATE TABLE `localidades` (
+  `idlocalidad` int(11) NOT NULL,
+  `nombrelocalidad` varchar(40) NOT NULL,
+  `idprovincialocalidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `localidades`
+--
+
+INSERT INTO `localidades` (`idlocalidad`, `nombrelocalidad`, `idprovincialocalidad`) VALUES
+(1, 'Moreno', 4),
+(2, 'Resistencia', 1),
+(3, 'Cafayate', 3),
+(4, 'Guaymayen', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `idpedido` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `lugarentrega` varchar(80) NOT NULL,
+  `localidad` int(11) NOT NULL,
+  `provincia` int(11) NOT NULL,
+  `codpostal` varchar(80) NOT NULL,
+  `formadepago` varchar(1) NOT NULL,
+  `tarjetatitular` varchar(40) NOT NULL,
+  `tarjetanumero` int(16) NOT NULL,
+  `tarjetavencimiento` varchar(10) NOT NULL,
+  `tarjetaclave` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE `provincias` (
+  `idprovincia` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`idprovincia`, `nombre`) VALUES
+(1, 'Chaco'),
+(3, 'Salta'),
+(4, 'Buenos Aires'),
+(5, 'Mendoza');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `idusuario` int(11) NOT NULL,
+  `usuario` varchar(20) NOT NULL,
+  `clave` varchar(20) NOT NULL,
+  `nombreyapellido` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idusuario`, `usuario`, `clave`, `nombreyapellido`) VALUES
+(1, 'prueba@gmail.com', '909090', 'Ignacio Benetucci'),
+(5, 'ignacio@gmail.com', 'ignacio123', 'Agustin Fernandez'),
+(6, '123@gmail.com', '12345', 'Maria Paez'),
+(7, 'agustin@gmail.com', '12345', 'Agustin Garcia'),
+(8, 'liliana@prueba.com', 'liliana123', 'Liliana Lopez');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD PRIMARY KEY (`idlocalidad`),
+  ADD UNIQUE KEY `LocProvincia` (`idlocalidad`),
+  ADD KEY `idprovincialocalidad` (`idprovincialocalidad`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`idpedido`);
+
+--
+-- Indices de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  ADD PRIMARY KEY (`idprovincia`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  MODIFY `idlocalidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `idpedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  MODIFY `idprovincia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `localidades`
+--
+ALTER TABLE `localidades`
+  ADD CONSTRAINT `localidades_ibfk_1` FOREIGN KEY (`idprovincialocalidad`) REFERENCES `provincias` (`idprovincia`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
